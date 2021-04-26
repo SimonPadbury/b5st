52009c82c54f6942fa2a69ee40e7032f4961d31c
Simon Padbury <simon.padbury@icloud.com>
2021-04-26 18:07:27 +0100

    v1.0

--- theme/js/b5st.js
+++ theme/js/b5st.js
@@ -1,34 +1,3 @@
 /*!
  * b5st JS
  */
-
-document.addEventListener('DOMContentLoaded', function () {
-
-	let commentListItem = document.querySelectorAll('.comment');
-	let commentReplyLink = document.querySelectorAll('.comment-reply-link');
-	let formElems = document.querySelectorAll('input[type=text]','input[type=email]','input[type=password]','textarea','select');
-	let submitButtons = document.querySelectorAll('input[type=submit]');
-
-	commentListItem.forEach(element => {
-		element.classList.add('card','shadow-sm','mb-3');
-	});
-
-	commentReplyLink.forEach(element => {
-		element.classList.add('btn','btn-primary');
-	});
-
-	formElems.forEach(element => {
-		element.classList.add('form-control');
-	});
-
-	submitButtons.forEach(element => {
-		element.classList.add('btn','btn-primary');
-	});
-
-	
-
-	// Pagination fix for ellipsis
-	//document.querySelectorAll('.pagination .dots').classList.add('page-link');
-	//document.querySelectorAll('.pagination .dots').parent.classList.add('disabled');
-
-}, false);
