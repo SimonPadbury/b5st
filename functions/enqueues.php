c752706c22af9166774754533fa9df3fe233fec2
Simon Padbury <simon.padbury@icloud.com>
2021-05-07 06:42:48 +0100

    Update enqueues.php

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -8,7 +8,7 @@ if ( ! function_exists('b5st_enqueues') ) {
 
 		// Styles
 
-		wp_register_style('bootstrap5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css', false, '5.0.0-beta3', null);
+		wp_register_style('bootstrap5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css', false, '5.0.0', null);
 		wp_enqueue_style('bootstrap5');
 
 		wp_register_style('bootstrapIcons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css', false, '1.4.1', null);
@@ -21,7 +21,7 @@ if ( ! function_exists('b5st_enqueues') ) {
 
 		// Scripts
 
-		wp_register_script('bootstrap5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js', false, '5.0.0-beta3', true);
+		wp_register_script('bootstrap5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js', false, '5.0.0', true);
 		wp_enqueue_script('bootstrap5');
 
 		// wp_register_script('theme', get_template_directory_uri() . '/theme/js/b5st.js', false, null, true);
