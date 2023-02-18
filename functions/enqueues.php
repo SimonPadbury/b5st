e8ce07d44f0281b15b09f02fa652d738f0d08560
Simon Padbury <simon.padbury@icloud.com>
2023-02-18 10:17:03 +0000

    Update enqueues.php

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -8,10 +8,10 @@ if ( ! function_exists('b5st_enqueues') ) {
 
 		// Styles
 
-		wp_register_style('bootstrap5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css', false, '5.2.1', null);
+		wp_register_style('bootstrap5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css', false, '5.2.3', null);
 		wp_enqueue_style('bootstrap5');
 
-		wp_register_style('bootstrapIcons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css', false, '1.9.1', null);
+		wp_register_style('bootstrapIcons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css', false, '1.10.2', null);
 		wp_enqueue_style('bootstrapIcons');
 
 		wp_enqueue_style( 'gutenberg-blocks', get_template_directory_uri() . '/theme/css/blocks.css' );
@@ -21,7 +21,7 @@ if ( ! function_exists('b5st_enqueues') ) {
 
 		// Scripts
 
-		wp_register_script('bootstrap5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js', false, '5.2.1', true);
+		wp_register_script('bootstrap5', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', false, '5.2.3', true);
 		wp_enqueue_script('bootstrap5');
 
 		// wp_register_script('theme', get_template_directory_uri() . '/theme/js/b5st.js', false, null, true);
